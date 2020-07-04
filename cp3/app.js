a998378e45b480d3f8dba887cad95a455c7cfc68
MatsudaTakuya <matsuda.takuya41@gmail.com>
2020-07-04 22:55:05 +0900

    commit

--- cp3/app.js
+++ cp3/app.js
@@ -4,7 +4,7 @@ const http = require("http");
 const fs = require("fs");
 const ejs = require("ejs");
 const url = require("url");
-const qs = require("querystring");
+const qs = require("querystring"); // 普通のテキストをパース処理するためのもの
 
 const index_page = fs.readFileSync("./index.ejs", "utf8");
 const other_page = fs.readFileSync("./other.ejs", "utf8");
@@ -15,12 +15,33 @@ let data = {
   msg: "no message...",
 };
 
+// Cookieの値を設定
+const setCookie = (key, value, response) => {
+  const cookie = escape(value);
+  response.setHeader("Set-Cookie", [key + "=" + cookie]);
+};
+
+const getCookie = (key, request) => {
+  const cookie_data =
+    request.headers.cookie != undefined ? request.headers.cookie : "";
+  const data = cookie_data.split(";");
+  for (let i in data) {
+    if (data[i].trim().startsWith(key + "=")) {
+      const result = data[i].trim().substring(key.length + 1);
+      return unescape(result);
+    }
+  }
+  return "";
+};
+
 const write_index = (request, response) => {
   msg = "伝言を表示します";
+  const cookie_data = getCookie("msg", request);
   const content = ejs.render(index_page, {
     title: "Index",
     content: msg,
     data: data,
+    cookie_data: cookie_data,
   });
   response.writeHead(200, { "Content-Type": "text/html" });
   response.write(content);
@@ -32,11 +53,15 @@ const response_index = (request, response) => {
     let body = "";
 
     request.on("data", (data) => {
+      // data = クライアントからデータを受け取ると発生するイベント
       body += data;
     });
 
     request.on("end", () => {
+      // end = データの受取が完了すると発生するイベント
       data = qs.parse(body);
+      // Cookieの保存
+      setCookie("msg", data.msg, response);
       write_index(request, response);
     });
   } else {
@@ -78,7 +103,7 @@ const response_other = (request, response) => {
 };
 
 const getFromClient = (request, response) => {
-  const url_parts = url.parse(request.url, true);
+  const url_parts = url.parse(request.url, true); // 第2引数trueでクエリパラメーターもparseする
   switch (url_parts.pathname) {
     case "/":
       response_index(request, response);
